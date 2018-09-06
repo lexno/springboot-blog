70ed977b908968f3a78b694f24a7740b8ba32447
lexno <lexno23@gmail.com>
2018-09-06 21:39:11 +0800

    -- 添加日志管理 -- 添加编码转换

--- src/main/java/tk/lexno/blog/controller/CategoryController.java
+++ src/main/java/tk/lexno/blog/controller/CategoryController.java
@@ -16,58 +16,63 @@ import tk.lexno.blog.entity.CategoryInfo;
 @RestController
 @RequestMapping("/api/category")
 public class CategoryController {
-	
-	/**
-	 * 获取所有分类信息
-	 * @return
-	 */
-	@ApiOperation("获取所有分类信息")
-	@GetMapping("/list")
-	public List<CategoryInfo> listAllCategoryInfo() {
-		return null;
-	}
-	
-	/**
-	 * 获取某一条分类信息
-	 * @param id 分类ID
-	 * @return
-	 */
-	@ApiOperation("获取某一条分类信息")
-	@ApiImplicitParam(name = "id", value = "分类ID", required = true, dataType = "Long")
-	@GetMapping("/{id}")
-	public CategoryInfo getCategoryInfoById(@PathVariable Long id) {
-		return null;
-	}
-	
-	/**
-	 * 更新/编辑某一条分类信息
-	 * @param id 分类ID
-	 * @return
-	 */
-	@ApiOperation("更新/编辑某一条分类信息")
-	@ApiImplicitParam(name = "id", value = "分类ID", required = true, dataType = "Long")
-	@PostMapping("/{id}")
-	public String updateCategoryInfoById(@PathVariable Long id) {
-		return null;
-	}
-	
-	/**
-	 * 增加分类信息
-	 * @return
-	 */
-	@ApiOperation("增加分类信息")
+
+    /**
+     * 获取所有分类信息
+     *
+     * @return
+     */
+    @ApiOperation("获取所有分类信息")
+    @GetMapping("/list")
+    public List<CategoryInfo> listAllCategoryInfo() {
+        return null;
+    }
+
+    /**
+     * 获取某一条分类信息
+     *
+     * @param id 分类ID
+     * @return
+     */
+    @ApiOperation("获取某一条分类信息")
+    @ApiImplicitParam(name = "id", value = "分类ID", required = true, dataType = "Long")
+    @GetMapping("/{id}")
+    public CategoryInfo getCategoryInfoById(@PathVariable Long id) {
+        return null;
+    }
+
+    /**
+     * 更新/编辑某一条分类信息
+     *
+     * @param id 分类ID
+     * @return
+     */
+    @ApiOperation("更新/编辑某一条分类信息")
+    @ApiImplicitParam(name = "id", value = "分类ID", required = true, dataType = "Long")
+    @PostMapping("/{id}")
+    public String updateCategoryInfoById(@PathVariable Long id) {
+        return null;
+    }
+
+    /**
+     * 增加分类信息
+     *
+     * @return
+     */
+    @ApiOperation("增加分类信息")
     @ApiImplicitParam(name = "name", value = "分类名称", required = true, dataType = "String")
     @PostMapping("/add")
     public String addCategoryInfo() {
         return null;
     }
 
-	/**
-	 * 删除分类信息
-	 * @param id
-	 * @return
-	 */
-	@ApiOperation("删除分类信息")
+    /**
+     * 删除分类信息
+     *
+     * @param id
+     * @return
+     */
+    @ApiOperation("删除分类信息")
     @ApiImplicitParam(name = "id", value = "分类ID", required = true, dataType = "Long")
     @DeleteMapping("/{id}")
     public String deleteCategoryInfo(@PathVariable Long id) {
